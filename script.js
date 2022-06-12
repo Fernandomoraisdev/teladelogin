2cd542c3ceb37d07a33b19cbb5f66652eb32fadb
fernandomoraisdev <fernandomoraisdev@gmail.com>
2022-06-11 21:36:23 -0300

    Primeiro

---
+++ script.js
@@ -0,0 +1,21 @@
+function clicou(){
+    alert("Você clicou com mouse em cima de login, vai mudar o nome de login para Registro");
+
+    document.getElementById("titulo").innerHTML="Registro";
+}
+
+function apertou(){
+    console.log("Apertou uma tecla");
+}
+
+function soltou(){
+    console.log("Apertou e soltou uma tecla");
+}
+
+function passou(){
+    console.log("colocou o mouse em cima");
+}
+
+function tirou(){
+    console.log("tirou o mouse de cima");
+}
